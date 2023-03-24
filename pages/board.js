33748d42cea0a4e21bac6c11c4adfd4d2e5c0e22
nijikazoku <119388243+nijikazoku@users.noreply.github.com>
2023-03-24 19:13:09 +0900

    .

--- pages/board.js
+++ pages/board.js
@@ -1,15 +1,5 @@
-import BoardDisplay from "../components/board/BoardDisplay";
-import BoardInput from "../components/board/BoardInput";
-import TestHeader from "../components/TestHeader";
-
 const board = () => {
-  return (
-    <>
-      <TestHeader />
-      <BoardDisplay />
-      <BoardInput />
-    </>
-  );
+  return <div>掲示板を表示</div>;
 };
 
 export default board;
