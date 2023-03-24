7b019a7a4abc1fe4988f057aa92c3a16d8e36ead
nijikazoku <119388243+nijikazoku@users.noreply.github.com>
2023-03-24 12:01:47 +0900

    up

--- components/displayBasketball/Betting.js
+++ components/displayBasketball/Betting.js
@@ -14,9 +14,12 @@ const Betting = ({ betList, setBetList, handleBet }) => {
     setBetList((prevBetList) => {
       const updatedBetList = prevBetList.map((bet) => {
         if (bet.matchId === matchId) {
-          const newBetNum = bet.betNum + amount * bet.incrementValue;
+          const newBetNum =
+            amount === 0 ? 0 : bet.betNum + amount * bet.incrementValue;
           if (newBetNum < 0) {
             return { ...bet, betNum: 0 };
+          } else if (newBetNum > 10000) {
+            return { ...bet, betNum: 10000 };
           } else {
             return { ...bet, betNum: newBetNum };
           }
@@ -24,6 +27,45 @@ const Betting = ({ betList, setBetList, handleBet }) => {
           return bet;
         }
       });
+      return updatedBetList;
+    });
+  };
+  const resetBetNum = (matchId) => {
+    setBetList((prevBetList) => {
+      const updatedBetList = prevBetList.map((bet) => {
+        if (bet.matchId === matchId) {
+          return { ...bet, betNum: 0 };
+        } else {
+          return bet;
+        }
+      });
+
+      const newBetNums = {
+        ...betNums,
+        [matchId]: 0,
+      };
+      setBetNums(newBetNums);
+
+      return updatedBetList;
+    });
+  };
+
+  const maxBetNum = (matchId) => {
+    setBetList((prevBetList) => {
+      const updatedBetList = prevBetList.map((bet) => {
+        if (bet.matchId === matchId) {
+          return { ...bet, betNum: 10000 };
+        } else {
+          return bet;
+        }
+      });
+
+      const newBetNums = {
+        ...betNums,
+        [matchId]: 10000,
+      };
+      setBetNums(newBetNums);
+
       return updatedBetList;
     });
   };
@@ -58,7 +100,8 @@ const Betting = ({ betList, setBetList, handleBet }) => {
   }, 0);
 
   return (
-    <div className="pb-4 fixed w-full top-[60px] h-[34rem] bg-opacity-90 bg-slate-700 overflow-y-auto">
+    <div className="fixed w-full top-0 h-full pb-[80px] bg-opacity-90 bg-slate-700 space-y-2 overflow-y-auto">
+      {/* CLOSEボタン */}
       <div onClick={handleBet} className="flex justify-end py-2 pr-5 space-x-3">
         <p>CLOSE</p>
         <img
@@ -67,6 +110,7 @@ const Betting = ({ betList, setBetList, handleBet }) => {
           alt=""
         />
       </div>
+      {/* 試合情報 */}
       <div className="space-y-5">
         {betList.map((bet, index) => {
           const rewards = Math.floor(bet.odds * bet.betNum);
@@ -95,28 +139,41 @@ const Betting = ({ betList, setBetList, handleBet }) => {
                   <div className="text-xl flex flex-col items-center space-x-2">
                     <div className="flex text-base">
                       <p className="text-gray-400">
-                        {bet.homeTeam} vs {bet.awayTeam}
+                        <span
+                          className={`${
+                            bet.oddsType === "oddsHome" &&
+                            "text-red-600 font-bold text-lg"
+                          }`}
+                        >
+                          {bet.homeTeam}
+                        </span>
+                        <span className="px-1">vs</span>
+                        <span
+                          className={`${
+                            bet.oddsType === "oddsAway" &&
+                            "text-red-600 font-bold text-lg"
+                          }`}
+                        >
+                          {bet.awayTeam}
+                        </span>
                       </p>
 
                       <div className="flex"></div>
-                    </div>
-                    <div className="flex space-x-3 items-center">
-                      <p> {bet.winTeam}</p>
-                      <span className="text-red-600 font-bold">WIN</span>
-                      <div className="font-bold text-lg text-green-500">
-                        {bet.odds}倍
-                      </div>
                     </div>
                   </div>
                 </div>
               </div>
 
               {/* 下のボックス */}
-              <div className="flex w-full  flex-col rounded-lg  from-gray-900 to-gray-600 bg-gradient-to-l justify-between items-center py-2">
+              <div className="flex w-full  flex-col rounded-lg  px-4 from-gray-900 to-gray-600 bg-gradient-to-l justify-between items-center py-2">
                 <div className="">
-                  <div>当たると:{rewards}pt</div>
+                  <div className="space-x-3 w-full flex justify-center">
+                    <span>当たると:{rewards}pt</span>
+                    <span className="text-green-600">{bet.odds}倍</span>
+                  </div>
                 </div>
-                <div className="flex flex-col space-y-2 justify-between items-center">
+                <div className="flex flex-col space-y-2 justify-between w-full items-center">
+                  {/* 増減量 */}
                   <div className="space-x-2">
                     <button
                       onClick={() => changeIncrementValue(bet.matchId, 1)}
@@ -159,25 +216,46 @@ const Betting = ({ betList, setBetList, handleBet }) => {
                       1000
                     </button>
                   </div>
-                  <div className="flex items-center space-x-2">
+                  {/* ベット数 */}
+                  <div className="flex items-center justify-around w-full  space-x-2">
+                    {/* リセットボタン */}
+                    <button
+                      onClick={() => resetBetNum(bet.matchId, betList)}
+                      className="border-1 px-2"
+                    >
+                      RESET
+                    </button>
+                    {/* マイナスボタン */}
                     <button
                       onClick={() =>
                         changeBetNum(-incrementValue, bet.matchId, betList)
                       }
                       className="border-1 px-2"
                     >
-                      <FcMinus size={23} />
+                      <img
+                        className="w-6"
+                        src="https://www.svgrepo.com/show/206436/delete-exit.svg"
+                        alt=""
+                      />
                     </button>
-                    <div className="text-xl text-center border w-[5rem]">
-                      {bet.betNum}
+                    <div className="text-xl text-center border shadow-lg py-1 w-[7rem]">
+                      {bet.betNum}pt
                     </div>
+                    {/* プラスボタン */}
                     <button
                       onClick={() =>
                         changeBetNum(+incrementValue, bet.matchId, betList)
                       }
                       className="border-1 px-2"
                     >
-                      <FcPlus size={23} />
+                      <FcPlus size={25} />
+                    </button>
+                    {/* マックスボタン */}
+                    <button
+                      onClick={() => maxBetNum(bet.matchId, betList)}
+                      className="border-1 px-2"
+                    >
+                      MAX
                     </button>
                   </div>
                 </div>
@@ -186,18 +264,23 @@ const Betting = ({ betList, setBetList, handleBet }) => {
           );
         })}
       </div>
-      <div>
-        <div className="text-xl font-bold mt-3">ベット数：{betList.length}</div>
-        <div className="text-xl font-bold mt-3">
-          合計ベット：{totalBetNums}pt
+      {/* ベット確認 */}
+      <div className="space-y-2">
+        <div className="text-center rounded-lg  w-[90%] flex flex-col justify-center items-center mx-auto border border-blue-900 bg-gradient-to-l from-blue-700 via-blue-800 to-gray-900">
+          <div className="text-xl font-bold py-1 w-full border-b">
+            ベット数：{betList.length}
+          </div>
+          <div className="border-b py-1 text-xl w-full font-bold">
+            合計ベット：{totalBetNums}pt
+          </div>
+          <div className="text-xl rounded-lg font-bold py-1  w-full ">
+            合計払い戻し: {totalRewards}pt
+          </div>
         </div>
-        <div className="text-xl font-bold mt-3">
-          合計払い戻し: {totalRewards}pt
-        </div>
+        <button className="mx-auto rounded-lg flex w-[90%] bg-red-600 py-2 justify-center items-center text-xl font-bold text-yellow-300">
+          ベットする
+        </button>
       </div>
-      <button className="w-full bg-blue-600 text-white py-2 text-xl font-bold">
-        ベットする
-      </button>
     </div>
   );
 };
